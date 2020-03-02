4669b1b8a1ed3aa283d831d20347cfaa1b774a6b
s17180 <s17180@LAB003-14>
2020-03-02 11:41:50 +0100

    jhgjhjh

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace Cwiczenia1
 {
@@ -7,6 +8,12 @@ namespace Cwiczenia1
         //sssssssss
        public static void Main(string[] args)
         {
+            string s = "ss";
+            var client = new HttpClient();
+            int g = 0;
+            for (int i = 0; i < 10; i++{
+                g += 1;
+            }
             Console.WriteLine("Hello World!");
         }
     }
